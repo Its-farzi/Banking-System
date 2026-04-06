7fea0c5992a0eca2159090e4c53db72cdd6bcb1f
Its-farzi <its_farzi@outlook.com>
2026-04-06 10:00:25 +0530

    changes

--- Basic_banking_system.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-printf("Welcome to The Farzi's Bank");
-printf("Please select your option");
-    return 0;
-}
---
+++ a.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+printf("aman");
+
+return 0;
+}
