38b841b9a4087ded1db957a0ac777b92ab54221d
Mohammad Faraz Qureshi <Its_farzi@outlook.com>
2026-04-06 15:28:16 +0530

    Update banking_system.c

--- banking_system.c
+++ banking_system.c
@@ -1,19 +1,1 @@
-#include<stdio.h>
-int account_number;
-int account_details;
-int account_balance;
 
-
-int main()
-{
-printf("Welcome to The Farzi's Bank\n");
-printf("Please select your option\n");
-printf("|**------------------------------------------**|\n");
-printf("|1. Open an Account.                           |\n");
-printf("|2. Deposite Money to Your Bank Account.       |\n");
-printf("|3. Withdrwal Money form Your Bank Account.    |\n");
-printf("|4. Check Your Current Account Balance.        |\n");
-printf("|4. Check Account Holder's Details.            |\n");
-
-    return 0;
-}
